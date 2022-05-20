67505f4afe4cb715fe6415fd7f212936fc5990ea
Sneha Parande <99521196+snehaparande@users.noreply.github.com>
2022-05-20 19:09:46 +0530

    Renamed function 'head' to 'cutText'

--- src/headLib.js
+++ src/headLib.js
@@ -3,10 +3,10 @@ const joinBy = (requiredLines, separator) => requiredLines.join(separator);
 
 const cutElements = (elements, noOfElements) => elements.slice(0, noOfElements);
 
-const head = (text, { separator, count }) => {
+const cutText = (text, { separator, count }) => {
   const allElements = splitBy(text, separator);
   const requiredEles = cutElements(allElements, count);
   return joinBy(requiredEles, separator);
 };
 
-exports.head = head;
+exports.cutText = cutText;
--- test/testHead.js
+++ test/testHead.js
@@ -1,56 +1,56 @@
 const assert = require('assert');
-const { head } = require('../src/headLib.js');
+const { cutText } = require('../src/headLib.js');
 
-describe('head', () => {
+describe('cutText', () => {
   it('Should return the same line when 1 line is given', () => {
-    assert.strictEqual(head('This is line', '\n', 10), 'This is line');
+    assert.strictEqual(cutText('This is line', '\n', 10), 'This is line');
     const text = 'This is another line';
-    assert.strictEqual(head(text, { separator: '\n', count: 10 }), text);
+    assert.strictEqual(cutText(text, { separator: '\n', count: 10 }), text);
   });
 
   it('Should return all the lines when 10 or less lines are given', () => {
     let text = 'line1\nline2\nline3\nline4\nline5';
-    assert.strictEqual(head(text, { separator: '\n', count: 10 }), text);
+    assert.strictEqual(cutText(text, { separator: '\n', count: 10 }), text);
     text = 'l1\nl2\nl3\nl4\nl5\nl6\nl7\nl8\nl9\nl10';
-    assert.strictEqual(head(text, { separator: '\n', count: 10 }), text);
+    assert.strictEqual(cutText(text, { separator: '\n', count: 10 }), text);
   });
 
   it('Should return first 10 lines of the given text', () => {
     const text = 'l1\nl2\nl3\nl4\nl5\nl6\nl7\nl8\nl9\nl10\nl11';
     const expected = 'l1\nl2\nl3\nl4\nl5\nl6\nl7\nl8\nl9\nl10';
-    assert.strictEqual(head(text, { separator: '\n', count: 10 }), expected);
+    assert.strictEqual(cutText(text, { separator: '\n', count: 10 }), expected);
   });
 
   it('Should return given number of lines from the text', () => {
     const text = 'l1\nl2\nl3\nl4\nl5';
-    assert.strictEqual(head(text, { separator: '\n', count: 2 }), 'l1\nl2');
+    assert.strictEqual(cutText(text, { separator: '\n', count: 2 }), 'l1\nl2');
   });
 
   it('Should return all lines when number of lines are more than total', () => {
     const text = 'l1\nl2\nl3\nl4\nl5';
-    assert.strictEqual(head(text, { separator: '\n', count: 10 }), text);
+    assert.strictEqual(cutText(text, { separator: '\n', count: 10 }), text);
   });
 
   it('Should return one character from the text', () => {
     let text = 'l1\nl2';
-    assert.strictEqual(head(text, { separator: '', count: 1 }), 'l');
+    assert.strictEqual(cutText(text, { separator: '', count: 1 }), 'l');
     text = 'This is line';
-    assert.strictEqual(head(text, { separator: '', count: 1 }), 'T');
+    assert.strictEqual(cutText(text, { separator: '', count: 1 }), 'T');
   });
 
   it('Should return 2 characters from the text', () => {
     const text = 'This is line';
-    assert.strictEqual(head(text, { separator: '', count: 2 }), 'Th');
+    assert.strictEqual(cutText(text, { separator: '', count: 2 }), 'Th');
   });
 
   it('Should return given number of characters from the text', () => {
     const text = 'line1\nline2';
-    assert.strictEqual(head(text, { separator: '', count: 7 }), 'line1\nl');
+    assert.strictEqual(cutText(text, { separator: '', count: 7 }), 'line1\nl');
   });
 
   it('Should return all characters when count exceeds total', () => {
     const text = 'line';
-    assert.strictEqual(head(text, { separator: '', count: 6 }), 'line');
+    assert.strictEqual(cutText(text, { separator: '', count: 6 }), 'line');
   });
 
 });
